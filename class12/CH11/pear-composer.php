5c760ba74af12435b590181b2472d18ed3cd3bce
asamamun <asamamun.web@gmail.com>
2023-01-08 19:04:39 +0600

    composer datetime fileio

---
+++ class12/CH11/pear-composer.php
@@ -0,0 +1,4 @@
+PEAR : PHP Extension and Application Repository <br>
+Composer: Composer is a tool for dependency management in PHP.
+<br>
+set php.exe in environment variable first and then install compsoer
---
+++ class12/CH12/date.php
@@ -0,0 +1,13 @@
+<h1>page 322 in book</h1>
+<h1>Set timezone in php.ini</h1>
+a:am/pm<?php echo date("a"); ?> <hr>
+A: AM/PM<?php echo date("A"); ?> <hr>
+d:08<?php echo date("d"); ?> <hr>
+D:Sun<?php echo date("D"); ?> <hr>
+e:Asia/Dhaka<?php echo date("e"); ?> <hr>
+F:January<?php echo date("F"); ?> <hr>
+G:18(24 hour format)<?php echo date("G"); ?> <hr>
+Timestamp : <?php echo date("U"); ?> <hr>
+Y-m-d H:i:s a:<?php echo date("Y-m-d H:i:s a"); ?> <hr>
+1673182809 time():<?php echo time(); ?> <hr>
+ 
